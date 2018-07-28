deb4e992d44ade8c5eadccd5e381cd468ea6f502
Stoner Life <31485689+9aylas@users.noreply.github.com>
2018-07-28 16:16:17 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  tagline: 'Full-stack Web Developer',
+  title: 'root@9aylas',  
+  author: 'Welcom', 
+  tagline: 'n/a',
   primaryColor: '#3498db', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald'
+    website: 'https://9aylas.netlify.com',
+    github: 'https://github.com/9aylas',
+    twitter: 'https://twitter.com/9aylas'
   }
 };
